33ca1885d33f37499f87f1a99e182996514b1532
Songju-Jeong <js0ngwn@gmail.com>
2023-06-14 11:48:04 +0900

    todolist 제목, 검색 부분 css

--- src/components/AddList.jsx
+++ src/components/AddList.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import "../css/addList.css";
 
 function AddList() {
   return (
-    <form>
-      <label>제목</label>&nbsp;
-      <input></input>
-      <label>내용</label>
-      <input></input>
-      <button>추가하기</button>
+    <form className="form-bg">
+      <div className="form-content">
+        <label>제목</label>
+        <input className="form-title" type="text"></input>
+        <label>내용</label>
+        <input className="form-text" type="text"></input>
+        <button>추가하기</button>
+      </div>
     </form>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AddList from "./AddList";
 import ViewList from "./ViewList";
+import "../css/todoList.css";
 
 function TodoList() {
   return (
     <div>
-      <header className="title">Songju TodoList✍</header>
+      <header className="title">My TodoList✍</header>
       <AddList />
     </div>
   );
